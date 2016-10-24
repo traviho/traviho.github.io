880b37d5e1d67b897a655819eb7499f8cf86c5ae
Travis Ho <thdarkblue@hotmail.com>
2016-10-24 14:30:35 -0400

    k

--- globe.js
+++ globe.js
@@ -21,7 +21,7 @@ DAT.Globe = function(container, opts) {
     c.setHSL( ( 0.6 - ( x * 0.5 ) ), 1.0, 0.5 );
     return c;
   };
-  //var imgDir = opts.imgDir || '/globe/';
+  //var imgDir = opts.imgDir || '';
 
   var Shaders = {
     'earth' : {
@@ -104,7 +104,7 @@ DAT.Globe = function(container, opts) {
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture('world.jpg');
+    uniforms['texture'].value = THREE.ImageUtils.loadTexture('opts.imgDir.world.jpg');
 
     material = new THREE.ShaderMaterial({
 
